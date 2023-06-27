af9a59e0ca23dc5b39c641eb5ec901b081b04231
ASHUTOSH PAWAR <80522156+gurrudev@users.noreply.github.com>
2023-06-27 14:38:20 +0530

    updated

--- BlogHub/src/components/Cards.jsx
+++ BlogHub/src/components/Cards.jsx
@@ -7,8 +7,8 @@ function Cards() {
         <div className="card">
             <img src={img1} alt="" />
             <h2>Title</h2>
-            <p>UserName</p>
-            
+            <p>UserName date</p>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure sapiente adipisci ea ullam dolorem sit voluptatum quasi esse cum, hic id, corrupti fuga? Vel excepturi corrupti necessitatibus. Sint, fuga mollitia!</p>
         </div>
     </div>
   )
