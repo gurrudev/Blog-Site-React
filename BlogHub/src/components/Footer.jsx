71239172aa13fab0115ea57c1e9d85d07409a4d4
ASHUTOSH PAWAR <80522156+gurrudev@users.noreply.github.com>
2023-06-28 12:42:32 +0530

    updated

--- BlogHub/src/components/Footer.jsx
+++ BlogHub/src/components/Footer.jsx
@@ -11,10 +11,10 @@ function Footer() {
     <div className='footer'>
       <div style={{ display: 'flex', gap: '8px' }}><span >Designed & Developed by</span> <a href='https://github.com/gurrudev'> ASHUTOSH</a></div>
       <div className='footer-icon'>
-        <a href=""><img src={InstagramIcon} alt="" srcset="" /></a>
-        <a href=""><img src={LinkedinIcon} alt="" srcset="" /></a>
-        <a href=""><img src={GithubIcon} alt="" srcset="" /></a>
-        <a href=""><img src={StackoverflowIcon} alt="" srcset="" /></a>
+        <a href="javascript: void(0)"><img src={InstagramIcon} alt="" srcset="" /></a>
+        <a href="javascript: void(0)"><img src={LinkedinIcon} alt="" srcset="" /></a>
+        <a href="javascript: void(0)"><img src={GithubIcon} alt="" srcset="" /></a>
+        <a href="javascript: void(0)"><img src={StackoverflowIcon} alt="" srcset="" /></a>
       </div>
     </div>
   </>)
