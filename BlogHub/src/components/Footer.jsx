ded50ed27d83d5f69372dbbd3b0ddefbf293abc4
ASHUTOSH PAWAR <80522156+gurrudev@users.noreply.github.com>
2023-06-28 11:04:09 +0530

    footer updated

--- BlogHub/src/components/Footer.jsx
+++ BlogHub/src/components/Footer.jsx
@@ -1,11 +1,21 @@
 import React from 'react'
 import './Footer.css'
+import InstagramIcon from '../assets/img/instagram-icon.png'
+import GithubIcon from '../assets/img/github-icon.png'
+import LinkedinIcon from '../assets/img/linkedin-icon.png'
+import StackoverflowIcon from '../assets/img/stackoverflow-icon.png'
+
 
 function Footer() {
   return (<>
     <div className='footer'>
-        <div style={{display:'flex', gap:'8px'}}><span >Designed & Developed by</span> <a href='https://github.com/gurrudev'> ASHUTOSH</a></div>
-        <div>AAA</div>
+      <div style={{ display: 'flex', gap: '8px' }}><span >Designed & Developed by</span> <a href='https://github.com/gurrudev'> ASHUTOSH</a></div>
+      <div className='footer-icon'>
+        <a href=""><img src={InstagramIcon} alt="" srcset="" /></a>
+        <a href=""><img src={LinkedinIcon} alt="" srcset="" /></a>
+        <a href=""><img src={GithubIcon} alt="" srcset="" /></a>
+        <a href=""><img src={StackoverflowIcon} alt="" srcset="" /></a>
+      </div>
     </div>
   </>)
 }
