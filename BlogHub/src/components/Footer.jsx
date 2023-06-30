6fad0c59baa2611df87810086e38f719dbccb0c1
ASHUTOSH PAWAR <80522156+gurrudev@users.noreply.github.com>
2023-06-30 12:50:55 +0530

    data

--- BlogHub/src/components/Footer.jsx
+++ BlogHub/src/components/Footer.jsx
@@ -8,17 +8,17 @@ import StackoverflowIcon from '../assets/img/stackoverflow-icon.png'
 function Footer() {
 
   return (
-  <>
-    <div className='footer'>
-      <div style={{ display: 'flex', gap: '8px' }}><span >Designed & Developed by</span> <a href='https://github.com/gurrudev'> ASHUTOSH</a></div>
-      <div className='footer-icon'>
-        <a href="javascript: void(0)"><img src={InstagramIcon} alt="" srcset="" /></a>
-        <a href="javascript: void(0)"><img src={LinkedinIcon} alt="" srcset="" /></a>
-        <a href="javascript: void(0)"><img src={GithubIcon} alt="" srcset="" /></a>
-        <a href="javascript: void(0)"><img src={StackoverflowIcon} alt="" srcset="" /></a>
+    <>
+      <div className='footer'>
+        <div style={{ display: 'flex', gap: '8px' }}><span >Designed & Developed by</span> <a href='https://github.com/gurrudev'> ASHUTOSH</a></div>
+        <div className='footer-icon'>
+          <a href="javascript: void(0)"><img src={InstagramIcon} alt="" srcset="" /></a>
+          <a href="javascript: void(0)"><img src={LinkedinIcon} alt="" srcset="" /></a>
+          <a href="javascript: void(0)"><img src={GithubIcon} alt="" srcset="" /></a>
+          <a href="javascript: void(0)"><img src={StackoverflowIcon} alt="" srcset="" /></a>
+        </div>
       </div>
-    </div>
-  </>
+    </>
   )
 }
 
