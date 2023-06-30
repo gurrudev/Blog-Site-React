a062906c0c277d56de426d2e6436fbc6f5706576
ASHUTOSH PAWAR <80522156+gurrudev@users.noreply.github.com>
2023-06-30 18:24:44 +0530

    updated

--- client/api/apiCalls.js
+++ client/api/apiCalls.js
@@ -2,10 +2,19 @@ import axios from "axios"
 
 export const getBlogsData = async() =>{
     try {
-        let returnData = await axios.get('https://embarrassed-boa-zipper.cyclic.app/api/blogs')
-           return returnData.data;
-        
+        let get_blogs = await axios.get('https://embarrassed-boa-zipper.cyclic.app/api/blogs')
+        return get_blogs.data;
     } catch (err) {
         console.log(err);
     }
 }
+
+export const getUsersData = async () =>{
+    try{
+        let get_users = await axios.get('https://embarrassed-boa-zipper.cyclic.app/api/users')
+        return get_users.data;
+    } catch (err) {
+        console.log(err);
+    }
+}
+
--- client/src/components/Cards.jsx
+++ client/src/components/Cards.jsx
@@ -1,10 +1,10 @@
 import React, { useEffect, useState } from 'react'
-import img1 from '../assets/img/img-1.jpg'
+
 import './Cards.css'
 import { getBlogsData } from '../../api/apiCalls'
 
 function Cards() {
-    const [cardData, setCardData] = useState(null);
+    const [cardBlogsData, setCardData] = useState(null);
     const dataFn = async () => {
         const data = await getBlogsData()
         setCardData(data.blogs_data)
@@ -13,12 +13,12 @@ function Cards() {
     useEffect(() => {
         dataFn()
     }, [])
-    // console.log(cardData);
+    
 
     return (
         <div className='card-container' >
             {
-                cardData?.map((item) => (
+                cardBlogsData?.map((item) => (
                     <div className="card">
                         <img src={'https://images.unsplash.com/photo-1520209759809-a9bcb6cb3241?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8aW1nfGVufDB8fDB8fHww&w=1000&q=80} alt="" '} />
                         <h2>{item.title}</h2>
