053c0582b65dd0036124b3a5644ee97567e86bf6
ASHUTOSH PAWAR <80522156+gurrudev@users.noreply.github.com>
2023-06-30 12:49:11 +0530

    data

--- BlogHub/src/components/Footer.jsx
+++ BlogHub/src/components/Footer.jsx
@@ -5,10 +5,10 @@ import GithubIcon from '../assets/img/github-icon.png'
 import LinkedinIcon from '../assets/img/linkedin-icon.png'
 import StackoverflowIcon from '../assets/img/stackoverflow-icon.png'
 
-
 function Footer() {
 
-  return (<>
+  return (
+  <>
     <div className='footer'>
       <div style={{ display: 'flex', gap: '8px' }}><span >Designed & Developed by</span> <a href='https://github.com/gurrudev'> ASHUTOSH</a></div>
       <div className='footer-icon'>
