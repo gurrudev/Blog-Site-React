6c7292041216328c826fc6b378f84f71c1ea4881
ASHUTOSH PAWAR <80522156+gurrudev@users.noreply.github.com>
2023-07-07 11:15:29 +0530

    updated

--- client/src/components/FiltersCards.jsx
+++ client/src/components/FiltersCards.jsx
@@ -3,27 +3,63 @@ import './FiltersCards.css'
 function FiltersCards() {
   return (
     <>
-    <div className="filter-container">
-      <div className="filter-cards-container">
-        <div className='main-card'>
-          <img src={'https://images.unsplash.com/photo-1520209759809-a9bcb6cb3241?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8aW1nfGVufDB8fDB8fHww&w=1000&q=80'} alt="" srcSet="" />
-          <h2>Title</h2>
-          <div className="user-details">
-            <p className='user-name'> ~ Ashutosh Pawar</p>
-            <p className='date'>June 28, 2023</p>
+      <div className="filter-container">
+        <div className="filter-cards-container">
+          <div className='main-card'>
+            <img src={'https://images.unsplash.com/photo-1520209759809-a9bcb6cb3241?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8aW1nfGVufDB8fDB8fHww&w=1000&q=80'} alt="" srcSet="" />
+            <h2>Title</h2>
+            <div className="user-details">
+              <p className='user-name'> ~ Ashutosh Pawar</p>
+              <p className='date'>June 28, 2023</p>
+            </div>
+            <p className='des'>description</p>
+            <a href="">View Post</a> <span className='underline'></span>
           </div>
-          <p className='des'>description</p>
-          <a href="">View Post</a> <span className='underline'></span>
+
+          <div className="sub-cards">
+            <div className="sub-cards-col">
+              <img src={'https://images.unsplash.com/photo-1520209759809-a9bcb6cb3241?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8aW1nfGVufDB8fDB8fHww&w=1000&q=80'} alt="" srcSet="" />
+              <div className="sub-card-title">
+                <h3>tleTitleTitleTit</h3>
+                <p className='date'>June 28, 2023</p>
+              </div>
+            </div>
+
+            <div className="sub-cards-col">
+              <img src={'https://images.unsplash.com/photo-1520209759809-a9bcb6cb3241?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8aW1nfGVufDB8fDB8fHww&w=1000&q=80'} alt="" srcSet="" />
+              <div className="sub-card-title">
+                <h3>eTitleTitleTitleTitle</h3>
+                <p className='date'>June 28, 2023</p>
+              </div>
+            </div>
+
+            <div className="sub-cards-col">
+              <img src={'https://images.unsplash.com/photo-1520209759809-a9bcb6cb3241?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8aW1nfGVufDB8fDB8fHww&w=1000&q=80'} alt="" srcSet="" />
+              <div className="sub-card-title">
+                <h3>tleTitleTitleTitle</h3>
+                <p className='date'>June 28, 2023</p>
+              </div>
+            </div>
+
+            <div className="sub-cards-col">
+              <img src={'https://images.unsplash.com/photo-1520209759809-a9bcb6cb3241?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8aW1nfGVufDB8fDB8fHww&w=1000&q=80'} alt="" srcSet="" />
+              <div className="sub-card-title">
+                <h3>Title</h3>
+                <p className='date'>June 28, 2023</p>
+              </div>
+            </div>
+          </div>
         </div>
 
-        <div className="sub-cards">
+        <div className="recent ">
           <div className="sub-cards-col">
             <img src={'https://images.unsplash.com/photo-1520209759809-a9bcb6cb3241?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8aW1nfGVufDB8fDB8fHww&w=1000&q=80'} alt="" srcSet="" />
             <div className="sub-card-title">
-              <h3>Title</h3>
+              <h3>TitleTitleTitle</h3>
               <p className='date'>June 28, 2023</p>
             </div>
           </div>
+
 
           <div className="sub-cards-col">
             <img src={'https://images.unsplash.com/photo-1520209759809-a9bcb6cb3241?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8aW1nfGVufDB8fDB8fHww&w=1000&q=80'} alt="" srcSet="" />
@@ -40,7 +76,7 @@ function FiltersCards() {
               <p className='date'>June 28, 2023</p>
             </div>
           </div>
-          
+
           <div className="sub-cards-col">
             <img src={'https://images.unsplash.com/photo-1520209759809-a9bcb6cb3241?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8aW1nfGVufDB8fDB8fHww&w=1000&q=80'} alt="" srcSet="" />
             <div className="sub-card-title">
@@ -51,42 +87,7 @@ function FiltersCards() {
         </div>
       </div>
 
-      <div className="recent">
-      <div className="sub-cards-col">
-            <img src={'https://images.unsplash.com/photo-1520209759809-a9bcb6cb3241?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8aW1nfGVufDB8fDB8fHww&w=1000&q=80'} alt="" srcSet="" />
-            <div className="sub-card-title">
-              <h3>Title</h3>
-              <p className='date'>June 28, 2023</p>
-            </div>
-          </div>
-      </div>
 
-      <div className="sub-cards-col">
-            <img src={'https://images.unsplash.com/photo-1520209759809-a9bcb6cb3241?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8aW1nfGVufDB8fDB8fHww&w=1000&q=80'} alt="" srcSet="" />
-            <div className="sub-card-title">
-              <h3>Title</h3>
-              <p className='date'>June 28, 2023</p>
-            </div>
-          </div>
-      </div>
-      <div className="sub-cards-col">
-            <img src={'https://images.unsplash.com/photo-1520209759809-a9bcb6cb3241?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8aW1nfGVufDB8fDB8fHww&w=1000&q=80'} alt="" srcSet="" />
-            <div className="sub-card-title">
-              <h3>Title</h3>
-              <p className='date'>June 28, 2023</p>
-            </div>
-          </div>
-      
-      <div className="sub-cards-col">
-      <img src={'https://images.unsplash.com/photo-1520209759809-a9bcb6cb3241?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8aW1nfGVufDB8fDB8fHww&w=1000&q=80'} alt="" srcSet="" />
-      <div className="sub-card-title">
-        <h3>Title</h3>
-        <p className='date'>June 28, 2023</p>
-      </div>
-    </div>
-
-      
-      
     </>
   )
 }
