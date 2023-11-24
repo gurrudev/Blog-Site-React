e7a9e2821798c2f440321d2dc7ae72b90cb84113
ASHUTOSH PAWAR <80522156+gurrudev@users.noreply.github.com>
2023-11-24 18:40:11 +0530

    cards updated

--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -1,37 +1,45 @@
 import React, { useEffect, useState } from 'react'
-
+// import { useAutoAnimate } from '@formkit/auto-animate'
 import './Cards.css'
 import { getBlogsData } from '../../api/apiCalls'
 
 function Cards() {
     const [cardBlogsData, setCardData] = useState(null);
+//    const [isLoading, setIsLoading] = useState(true)
+//    const { animate } = useAutoAnimate()
     const dataFn = async () => {
         const data = await getBlogsData()
         setCardData(data.blogs_data)
+        // setIsLoading(false)
     }
 
     useEffect(() => {
-        dataFn()
+        const timer = setTimeout(()=>{
+            dataFn()
+        },2000)
+        return ()=> clearTimeout(timer)
     }, [])
 
     
     // For card 
     return (
         <div className='card-container' >
+            {/* <main ref={animate} > */}
             {
                 cardBlogsData?.filter((item, index) => index < 8)?.map((item) => (
-                    <div className="card">
+                    <div className="card ">
                         <img src={'https://images.unsplash.com/photo-1520209759809-a9bcb6cb3241?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8aW1nfGVufDB8fDB8fHww&w=1000&q=80'} alt=""  />
                         <h2>{item.title}</h2>
                         <div className="user-details">
-                            <p className='user-name'> ~ Ashutosh Pawar</p>
-                            <p className='date'>June 28, 2023</p>
+                            <p className='date text-[15px]'> ~ Ashutosh Pawar</p>
+                            <p className='date text-[15px]'>June 28, 2023</p>
                         </div>
                         <p className='des'>{item.description}</p>
                         <a href="">View Post</a> <span className='underline'></span>
                     </div>
                 ))
             }
+            {/* </main> */}
         </div>
     )
 }
