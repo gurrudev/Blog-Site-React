bc2f9ace8cf7ed6ff6833d068d4424ea0bac8bca
ASHUTOSH PAWAR <80522156+gurrudev@users.noreply.github.com>
2023-11-28 10:33:33 +0530

    signup page

--- src/pages/Signup.jsx
+++ src/pages/Signup.jsx
@@ -22,15 +22,19 @@ const Signup = () => {
                             <form class="space-y-4 md:space-y-6" action="#">
                                 <div>
                                     <label for="name" class="block mb-2 text-sm font-medium text-gray-900 ">Your Name</label>
-                                    <input type="name" name="name" id="name" class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-sm focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5" placeholder="name@company.com" required="" />
+                                    <input type="name" name="name" id="name" class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-sm focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5" placeholder="Jon Doe" required="" />
                                 </div>
                                 <div>
-                                    <label for="email" class="block mb-2 text-sm font-medium text-gray-900 ">Your email</label>
+                                    <label for="email" class="block mb-2 text-sm font-medium text-gray-900 ">Your Email</label>
                                     <input type="email" name="email" id="email" class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-sm focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5" placeholder="name@company.com" required="" />
                                 </div>
                                 <div>
                                     <label for="password" class="block mb-2 text-sm font-medium text-gray-900 ">Password</label>
                                     <input type="password" name="password" id="password" placeholder="••••••••" class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-sm focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 " required="" />
+                                </div>
+                                <div>
+                                    <label for="confpassword" class="block mb-2 text-sm font-medium text-gray-900 ">Confirm Password</label>
+                                    <input type="password" name="confpassword" id="confpassword" placeholder="••••••••" class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-sm focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 " required="" />
                                 </div>
                                 
                                 <button type="submit" class="w-full text-white bg-[black] hover:bg-[#36363b] focus:outline-none  font-medium rounded-sm text-lg px-5 py-2.5 text-center ">Sign up</button>
