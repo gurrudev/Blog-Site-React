528a6f46a01334f62f8de177ae27b43eb2bc9eb8
ASHUTOSH PAWAR <80522156+gurrudev@users.noreply.github.com>
2023-11-29 16:08:17 +0530

    updated

--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -26,28 +26,29 @@ function Cards() {
     // For card 
     return (
         <>
-        <main ref={animate} >
-        
-            <div className='card-container' >
-                {
-                    isLoading ? Array(8).fill(0).map((d,i) => (<HomeSekeleton key={i}/>)):
-                    cardBlogsData?.filter((item, index) => index < 8)?.map((item) => (
-                        <div className="card ">
-                            <img src={'https://images.unsplash.com/photo-1520209759809-a9bcb6cb3241?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8aW1nfGVufDB8fDB8fHww&w=1000&q=80'} alt="" />
-                            <h2 className='uppercase'>{item.title}</h2>
-                            <div className="user-details">
-                                <p className='date text-[15px]'> ~ Ashutosh Pawar</p>
-                                <p className='date text-[15px]'>June 28, 2023</p>
-                            </div>
-                            <p className='des text-justify'>{truncate(item.description)}</p>
-                            <a href="">View Post</a> <span className='underline'></span>
-                        </div>
-                    ))
-                }
-            </div>
-        </main>
+            <main ref={animate} >
 
-        <Cover/>
+                <h1 className='text-[23px] mt-[20px] ml-11 mb-[-20px]'>"Recent"</h1>
+                <div className='card-container' >
+                    {
+                        isLoading ? Array(8).fill(0).map((d, i) => (<HomeSekeleton key={i} />)) :
+                            cardBlogsData?.filter((item, index) => index < 8)?.map((item) => (
+                                <div className="card ">
+                                    <img src={'https://images.unsplash.com/photo-1520209759809-a9bcb6cb3241?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8aW1nfGVufDB8fDB8fHww&w=1000&q=80'} alt="" />
+                                    <h2 className='uppercase'>{item.title}</h2>
+                                    <div className="user-details">
+                                        <p className='date text-[15px]'> ~ Ashutosh Pawar</p>
+                                        <p className='date text-[15px]'>June 28, 2023</p>
+                                    </div>
+                                    <p className='des text-justify'>{truncate(item.description)}</p>
+                                    <a href="">View Post</a> <span className='underline'></span>
+                                </div>
+                            ))
+                    }
+                </div>
+            </main>
+
+            <Cover />
 
         </>
     )
