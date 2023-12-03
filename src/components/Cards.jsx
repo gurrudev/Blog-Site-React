7097a8dbc959c813320f8c430cf56a06c9ce9c48
ASHUTOSH PAWAR <80522156+gurrudev@users.noreply.github.com>
2023-12-03 16:54:48 +0530

    home updated

--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -1,26 +1,53 @@
 import React, { useEffect, useState } from 'react'
+import { useAutoAnimate } from '@formkit/auto-animate/react'
 import './Cards.css'
+import { getBlogsData } from '../../api/apiCalls'
+import HomeSekeleton from './Skeleton/HomeSekeleton'
 import truncate from '../../utils/Truncate'
 
-function Cards({ imgUrl, title, description, clickAction }) {
+function Cards({}) {
+    const [cardBlogsData, setCardData] = useState(null);
+    const [isLoading, setIsLoading] = useState(true)
+    const { animate } = useAutoAnimate()
+    const dataFn = async () => {
+        const data = await getBlogsData()
+        setCardData(data.blogs_data)
+        setIsLoading(false)
+    }
+
+    useEffect(() => {
+        const timer = setTimeout(() => {
+            dataFn()
+        }, 2000)
+        return () => clearTimeout(timer)
+    }, [])
+
 
     // For card 
     return (
         <>
-            <div className='card-container'>
+            <main ref={animate} >
 
-                <div className="card ">
-                    <img src={imgUrl} alt="" />
-                    <h2 className='uppercase'>{title}</h2>
-                    <div className="user-details">
-                        <p className='date text-[15px]'> ~ Ashutosh Pawar</p>
-                        <p className='date text-[15px]'>June 28, 2023</p>
-                    </div>
-                    <p className='des text-justify'>{truncate(description)}</p>
-                    <a href="">View Post</a> <span className='underline'></span>
+                <div className='card-container'>
+                    {
+                        isLoading ? Array(8).fill(0).map((d, i) => (<HomeSekeleton key={i} />)) :
+                            cardBlogsData?.filter((item, index) => index < 8)?.map((item) => (
+                                <div className="card ">
+                                    <img src={'https://images.unsplash.com/photo-1520209759809-a9bcb6cb3241?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8aW1nfGVufDB8fDB8fHww&w=1000&q=80'} alt="" />
+                                    <h2 className='uppercase'>{item.title}</h2>
+                                    <div className="user-details">
+                                        <p className='date text-[15px]'> ~ Ashutosh Pawar</p>
+                                        <p className='date text-[15px]'>June 28, 2023</p>
+                                    </div>
+                                    <p className='des text-justify'>{truncate(item.description)}</p>
+                                    <a href="">View Post</a> <span className='underline'></span>
+                                </div>
+                            ))
+                    }
                 </div>
+            </main>
 
-            </div>
+
         </>
     )
 }
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,43 +1,13 @@
-import React, { useEffect, useState } from 'react'
+import React from 'react'
 import Banner from '../components/Banner'
 import Cards from '../components/Cards'
 import FilterContainer from '../components/FilterContainer'
-import { getBlogsData } from '../../api/apiCalls'
-import HomeSekeleton from '../components/Skeleton/HomeSekeleton'
 import Cover from '../components/Cover'
-import { useAutoAnimate } from '@formkit/auto-animate/react'
-
 const Home = () => {
-    const [cardBlogsData, setCardData] = useState(null);
-    const [isLoading, setIsLoading] = useState(true)
-    const { animate } = useAutoAnimate()
-
-    const dataFn = async () => {
-        const data = await getBlogsData()
-        setCardData(data.blogs_data)
-        setIsLoading(false)
-    }
-
-
-    useEffect(() => {
-        const timer = setTimeout(() => {
-        dataFn()
-        }, 2000)
-        return () => clearTimeout(timer)
-    }, [])
-
-
     return (
-        <div key={''}>
+        <div className=''>
             <Banner />
-            {
-                isLoading ? Array(8).fill(0).map((d, i) => (<HomeSekeleton key={i} />)) :
-                    cardBlogsData?.filter((item, index) => index < 8)?.map((item) => (
-                        <main ref={animate}>
-                            <Cards imgUrl={'https://images.unsplash.com/photo-1520209759809-a9bcb6cb3241?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8aW1nfGVufDB8fDB8fHww&w=1000&q=80'} title={item.title} description={item.description} clickAction={''} />
-                        </main>
-                    ))
-            }
+            <Cards />
             <Cover />
             <FilterContainer />
         </div>
