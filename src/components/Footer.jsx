cea757a184fd436e5d9966dbc693c996ca2922c1
ASHUTOSH PAWAR <80522156+gurrudev@users.noreply.github.com>
2023-12-03 18:07:08 +0530

    card updated

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -13,7 +13,7 @@ function Footer() {
         <p className="mb-4 text-sm text-center text-gray-200 dark:text-gray-200 sm:mb-0">
           <span >Designed & Developed by</span> <a className="text-base hover:underline text-[white] text-" href='https://github.com/gurrudev'> ASHUTOSH</a>
         </p>
-        <div class="flex justify-center items-center space-x-1">
+        <div className="flex justify-center items-center space-x-1">
           <div className='footer-icon'>
             <a href="javascript: void(0)"><img src={InstagramIcon} alt="" srcSet="" /></a>
             <a href="javascript: void(0)"><img src={LinkedinIcon} alt="" srcSet="" /></a>
