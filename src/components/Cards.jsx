0a51dade9e130520f2dc5641399bce8d1fdd9366
ASHUTOSH PAWAR <80522156+gurrudev@users.noreply.github.com>
2023-12-25 16:39:57 +0530

    card updated

--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -3,8 +3,9 @@ import { useAutoAnimate } from '@formkit/auto-animate/react'
 import './Cards.css'
 import HomeCardSekeleton from './Skeleton/HomeCardSekeleton'
 import truncate from '../../utils/Truncate'
+import { CiImageOn } from "react-icons/ci";
 
-function Cards({ cardsData, totalCards, cardAction}) {
+function Cards({ cardsData, totalCards, cardAction }) {
 
     const [isLoading, setIsLoading] = useState(true)
     const { animate } = useAutoAnimate()
@@ -30,7 +31,22 @@ function Cards({ cardsData, totalCards, cardAction}) {
                         isLoading ? Array(totalCards).fill(0).map((d, i) => (<HomeCardSekeleton key={i} />)) :
                             cardsData?.filter((item, index) => index < totalCards)?.map((item, index) => (
                                 <div className="card " key={index}>
-                                    <img src={'https://images.unsplash.com/photo-1520209759809-a9bcb6cb3241?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8aW1nfGVufDB8fDB8fHww&w=1000&q=80'} alt="" />
+                                    {/* <img src={'https://images.unsplash.com/photo-1520209759809-a9bcb6cb3241?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8aW1nfGVufDB8fDB8fHww&w=1000&q=80'} alt="" /> */}
+
+                                    <div className="relative">
+                                        <img className='' src={"https://images.unsplash.com/photo-1520209759809-a9bcb6cb3241?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8aW1nfGVufDB8fDB8fHww&w=1000&q=80"} alt="Your Image" />
+                                        <div className="absolute inset-0">
+                                            <div className='flex justify-between p-4 text-white text-lg'>
+                                                <div></div>
+                                                <div className='bg-white bg-opacity-20 backdrop-blur-lg rounded-[100%]'>
+                                                    <div className='p-1.5'>
+                                                        <CiImageOn />
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+
                                     <h2 className='uppercase'>{item.title}</h2>
                                     <div className="user-details">
                                         <p className='date text-[15px]'> ~ Ashutosh Pawar</p>
