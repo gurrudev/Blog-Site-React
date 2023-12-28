6dc89301cd58677d6503ec5bf73a332946d8eb75
ASHUTOSH PAWAR <80522156+gurrudev@users.noreply.github.com>
2023-12-28 19:10:26 +0530

    Text Editor Added

--- src/pages/WriteBlog.jsx
+++ src/pages/WriteBlog.jsx
@@ -1,8 +1,20 @@
-import React from 'react'
+import React,{useState} from 'react'
 import _avatar from '../assets/img/img_avatar.png'
-import Editor from '../components/Editor'
+import ReactQuill from 'react-quill';
+import 'react-quill/dist/quill.snow.css';
 
 export const WriteBlog = () => {
+  const [editorValue, setEditorValue] = useState('');
+
+  const editorModules = {
+    toolbar: [
+      [{ header: [1, 2, 3, 4, 5, 6, false] }],
+      ['bold', 'italic', 'link', 'blockquote', 'code'],
+      [{ list: 'ordered' }, { list: 'bullet' }, {indent : '+1'}],
+    ]
+  }
+
+  // console.log(editorValue)
   return (
     <>
       <div className='p-8 pt-4 lg:p-52 lg:pt-4 md:p-20 md:pt-4'>
@@ -22,16 +34,22 @@ export const WriteBlog = () => {
           <div className='pt-10'>
             <div>
               <div className="mb-6">
-                {/* <label for="email" className="block mb-2 text-sm font-medium text-gray-900">Email address</label> */}
-                <input type="text" id="title" className="text-gray-900 text-2xl form-heading rounded-lg outline-none block w-full" placeholder="Title" required />
-              </div>
-              <div className="mb-6">
                 {/* <label for="password" className="block mb-2 text-sm font-medium text-gray-900">Password</label> */}
-                <input type="text" id="image_link" className="text-gray-900 text-sm rounded-lg outline-none block w-full" placeholder="Your wallpaper link" required />
+                <input type="text" id="image_link" className="text-gray-900 text-base rounded-lg outline-none block w-full rq-form-element" placeholder="Your wallpaper link" required autoFocus/>
               </div>
               <div className="mb-6">
-                {/* <label for="confirm_password" className="block mb-2 text-sm font-medium text-gray-900">Confirm password</label> */}
-                <Editor/>
+                {/* <label for="email" className="block mb-2 text-sm font-medium text-gray-900">Email address</label> */}
+                <input type="text" id="title" className="text-gray-900 text-3xl form-heading rounded-lg outline-none block w-full" placeholder="Title" required />
+              </div>
+              <div className="mb-6">
+                <h2 className="block mb-2 text-base font-semibold form-text text-gray-900">Tell your story...</h2>
+                <ReactQuill 
+                  theme="snow" 
+                  value={editorValue} 
+                  onChange={setEditorValue} 
+                  className='form-text'
+                  modules={editorModules}  
+                />
               </div>
               
             </div>
