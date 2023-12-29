db458589f56a71490e6b24abf8384146b30b4bd9
ASHUTOSH PAWAR <80522156+gurrudev@users.noreply.github.com>
2023-12-29 19:04:40 +0530

    User Profile Added

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -1,19 +1,19 @@
 import React from 'react'
 import './Banner.css'
 import Navbar from './Navbar'
-import { Link } from 'react-router-dom'
+import { Link, useNavigate } from 'react-router-dom'
 import banner_img from '../assets/img/banner.jpg'
 function Banner() {
 
-    return (
+    const navigate = useNavigate()
 
+    return (
         <div className="banner">
             <Navbar />
-
             <div className="banner-text">
                 <h1 className='text-center'>Experience the beauty of diverse perspectives</h1>
-                <h3><Link to={'/write-blog'}>Write Your thoughts</Link></h3>
-                <button className='banner-btn bg-white text-slate-800 rounded-sm pt-2 pb-2 pl-4 pr-4 hover:bg-slate-100 mt-5 text-sm'>Get Started</button>
+                <h3 className=''><Link className='text-white' to={'/write-blog'}>Write Your thoughts</Link></h3>
+                <button className='banner-btn bg-white text-slate-800 rounded-sm pt-2 pb-2 pl-4 pr-4 hover:bg-slate-100 mt-5 text-sm' onClick={()=>navigate('/login')}>Get Started</button>
             </div>
             <div className="overlay"></div>
             <div className="custom-clip-path">
--- src/pages/UserProfile.jsx
+++ src/pages/UserProfile.jsx
@@ -1,8 +1,83 @@
 import React from 'react'
+import _avatar from '../assets/img/img_avatar.png'
+import { FaArrowLeft } from "react-icons/fa6";
+import { useNavigate } from 'react-router-dom';
+import { MdOutlineDateRange } from "react-icons/md";
+import { MdOutlineLocationOn } from "react-icons/md";
 
 function UserProfile() {
+
+  const navigate = useNavigate()
+
   return (
-    <div>UserProfile</div>
+    <>
+
+      <div class="p-7 sm:p-20  sm:pt-6">
+        <div className="space-y-12 ">
+          <div className="">
+            <div className='flex justify-between'>
+              <h1 className="leading-7 text-3xl text-gray-900 mt-1 logo">BlogHub</h1>
+              <div className='flex justify-between gap-2'>
+                {/* <button className='text-white text-sm bg-black p-3 pt-0.5 pb-0.5 lg:pl-3 lg:pt-0 lg:pb-0 rounded-sm form-text'>Post</button> */}
+                <img className='rounded-full w-8' src={_avatar} alt="" srcSet="" />
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div class="md:flex no-wrap md:-mx-2 mt-10">
+
+          <div class="w-full md:w-3/12 md:mx-2">
+            <div className='mb-10 text-2xl text-gray-900 cursor-pointer' onClick={()=>navigate('/')}><FaArrowLeft /></div>
+
+            <div class="">
+              <div class="">
+                <img class="h-20 w-20 rounded-full "
+                  src={_avatar}
+                  alt="" />
+              </div>
+              <h1 class="text-gray-900 font-bold text-xl form-heading leading-8 my-1">Jane Doe</h1>
+              <h3 class="text-gray-600 font-bold form-text leading-6">Owner at Her Company Inc.</h3>
+              <p class="text-sm text-gray-500 form-text hover:text-gray-600 leading-6 ">Lorem ipsum dolor sit amet
+                consectetur adipisicing e Reprehenderit, eligendi dolorum sequi illum qui unde aspernatur non deserunt</p>
+              <div className='flex gap-2'>
+
+              <div className='text-gray-600 text-sm form-text mt-1 flex gap-1'>
+                <MdOutlineDateRange className='mt-1' />
+                <p>Joined June 2012</p>
+              </div>
+              <div className='text-gray-600 text-sm form-text mt-1 flex gap-1'>
+                <MdOutlineLocationOn className='mt-1' />
+                <p>Mumbai</p>
+              </div>
+              </div>
+            </div>
+
+            <div class="my-4"></div>
+
+            <div class="pt-3 pb">
+              <div class="flex items-center mb-2 space-x-3 font-semibold text-gray-900 text-xl leading-8">
+                <span>Skills</span>
+              </div>
+              <div class="flex gap-1.5 form-text flex-wrap text-sm">
+                <span className=' py-0.5 px-3 bg-orange-900 text-white rounded-sm'>Programming</span>
+                <span className=' py-0.5 px-3 bg-orange-900 text-white rounded-sm'>Writing</span>
+                <span className=' py-0.5 px-3 bg-orange-900 text-white rounded-sm'>Python</span>
+                <span className=' py-0.5 px-3 bg-orange-900 text-white rounded-sm'>Java</span>
+                <span className=' py-0.5 px-3 bg-orange-900 text-white rounded-sm'>Web Development</span>
+              </div>
+            </div>
+          </div>
+
+          <div class="w-full  border">
+
+            
+
+          </div>
+        </div>
+      </div>
+
+    </>
   )
 }
 
