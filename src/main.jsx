917cbbdaf98983fab96749199acf4d9aedb88451
ASHUTOSH PAWAR <80522156+gurrudev@users.noreply.github.com>
2024-01-04 22:28:27 +0530

    toast updated

--- src/main.jsx
+++ src/main.jsx
@@ -4,25 +4,11 @@ import App from './App.jsx'
 import './index.css'
 import { store } from "./redux/app/store";
 import { Provider } from "react-redux";
-import { transitions, positions, Provider as AlertProvider } from 'react-alert'
-import AlertTemplate from 'react-alert-template-basic'
-
-// optional configuration
-const options = {
-  // you can also just use 'bottom center'
-  position: positions.TOP_RIGHT,
-  timeout: 5000,
-  offset: '30px',
-  // you can also just use 'scale'
-  transition: transitions.SCALE
-}
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <AlertProvider template={AlertTemplate} {...options}>
       <Provider store={store}>
         <App />
       </Provider>
-    </AlertProvider>
   </React.StrictMode>,
 )
